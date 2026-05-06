273f94f561b0e23897137a405f8a690f2e0c7787
RobelAlemayehu <robelalemayehu838@gmail.com>
2026-05-06 10:38:44 +0300

    feat: implement create and get own investor profile functions in controller

---
+++ server/controllers/investorController.js
@@ -0,0 +1,38 @@
+const investorService = require('../services/investorService')
+
+
+const createInvestorProfile = async (req, res) => {
+
+    try {
+        const profile = await investorService.createProfile({
+            ...req.body,
+            user: req.user.id
+        })
+
+        res.status(200).json({
+            success: true,
+            data: profile
+        })
+    } catch (error) {
+        res.statsu(500).josn({
+            message: error.message
+        })
+    }
+}
+
+
+const getOwnProfile = async (req, res) => {
+    try {
+        const profile = await investorService.getOwnProfile(req.user.id)
+
+        res.status(200).json({
+            profile
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+
